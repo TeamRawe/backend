91a5f53d098e3f8391fc85442bf7d6c1eba50db1
Eugene_ABT <81954052+About-to-break@users.noreply.github.com>
2024-11-21 21:47:49 +0300

    feat(): 1. Авторизация заменена на сессионную по cookie + CSRF. 2. Улучшен декоратор доступа по назначению

--- projects/views.py
+++ projects/views.py
@@ -7,6 +7,6 @@ from rest_framework.decorators import api_view, permission_classes
 @permission_classes([IsAuthenticated])
 @role_required(['ADMIN'])
 @assignment_required(['ACTIVE', 'FREEZED'])
-def test_assign(request):
+def test_assign(request, project_id, stage_id):
     user = request.user
     return Response({"message": f"Hello, {user.first_name}! You are allowed on this target"})
